9f4484e312bf40b732a9f872276adbfde2e41320
AmirMc231 <amirmc231@gmail.com>
2022-05-01 04:27:19 -0700

    Lesson B

---
+++ Assets/Scripts/SpawnMangager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMangager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 8.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 spawnpos2 = GenerateSpawnPosition();
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRange, spawnRange);
+        float zPos = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnpos = new Vector3(xPos, enemyPrefab.transform.position.y, zPos);
+        return spawnpos;
+    }
+
+    void Update()
+    {
+
+    }
+}
