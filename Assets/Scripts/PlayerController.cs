89f665ef66169d00bea81b9593d507f105d21d62
AmirMc231 <amirmc231@gmail.com>
2022-05-08 20:51:56 -0700

    Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rbPlayer;
     public float speed = 10.0f;
+    public float powerUpSpeed = 10.0f;
     Renderer render;
     GameObject focalPoint;
+    public GameObject PowerUpIndicator;
+
+
+    bool hasPowerUp = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,5 +37,38 @@ public class PlayerController : MonoBehaviour
         {
             render.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
         }
+        PowerUpIndicator.transform.position = transform.position;
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Power_up"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpDur());
+            PowerUpIndicator.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with " + collision.gameObject + " with powerup set to " + hasPowerUp);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpDur()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        PowerUpIndicator.SetActive(false);
+    }
+
 }
